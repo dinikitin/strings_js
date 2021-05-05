3380065368ff83c86abc001cc5e0a27f6d683d7a
Dmitrii Nikitin <dnikitin.exceedteam@gmail.com>
2021-05-05 17:12:42 +0300

    made changes

--- string.js
+++ string.js
@@ -2,18 +2,18 @@
 
 const string = 'Я изучаю JavaScript'
 
-const first_char_1 = string[0]
-const first_char_2 = string.slice(0, 1)
+const firstChar1 = string[0]
+const firstChar2 = string.slice(0, 1)
 
-const third_char_1 = string.slice(2, 3)
-const hird_char_2 = string[2]
+const thirdChar1 = string.slice(2, 3)
+const hirdChar2 = string[2]
 
-const last_char_1 = string[string.length - 1]
-const last_char_2 = string.slice(-1)
+const lastChar1 = string[string.length - 1]
+const lastChar2 = string.slice(-1)
 
-console.log(`Первый символ строки - ${first_char_1}`)
-console.log('Третий символ строки - ', third_char_1)
-console.log('Последний символ строки - ', last_char_1)
+console.log(`Первый символ строки - ${firstChar1}`)
+console.log('Третий символ строки - ', thirdChar1)
+console.log('Последний символ строки - ', lastChar1)
 
 //    -------Задача 2------
 
