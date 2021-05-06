739eeddb24e7aebb2061f2376ff52034d287a0dc
Dmitrii Nikitin <dnikitin.exceedteam@gmail.com>
2021-05-06 12:20:31 +0300

    Task done!

---
+++ arrayFilter.js
@@ -0,0 +1,23 @@
+let list = [31, 10, 'chiken', 9, 'fish', 10]
+
+function isStr(str) {
+    return isNaN(str)
+}
+
+let listString = list.filter((str)=>{
+    return isNaN(str)
+})
+
+let listNumber = list.filter((num)=>{
+    return num === 10
+})
+
+listString.forEach((item, array)=>{
+    console.log(item)
+})
+
+console.log('----------')
+
+listNumber.forEach((item, index, array)=>{
+    console.log(item, '-', index)
+})
---
+++ arrayFindDel.js
@@ -0,0 +1,29 @@
+// -----Task_1-----
+
+const animal = ['cat', 'dog', 'parrot', 'horse']
+
+animal.forEach((item, index)=>{
+    return item==='parrot'? console.log(`This is parrot! - ${index}`): console.log(`This is not parrot ${index}`)
+})
+console.log('-------')
+const parrot = animal.indexOf('parrot')
+console.log(parrot)
+
+
+// -----Task_2-----
+
+animal.push('fish', 'chiken', 'lion')
+
+const pos = animal.length-2, k = 2
+animal.splice(pos, k)              // removing the last element
+
+animal.shift()                     // removing the first element
+//animal.splice(0, 1)
+
+let indexFish = animal.indexOf('fish')
+animal.splice(indexFish, 1)
+
+animal.forEach((item, index)=>{
+    console.log(`${item} - ${index}`)
+})
+
