55ddc07ddaa2123da46158b391241985769954c1
Dmitrii Nikitin <dnikitin.exceedteam@gmail.com>
2021-05-07 11:55:14 +0300

    Task done

--- obj.js
+++ obj.js
@@ -5,13 +5,36 @@ const person = {
     location: 'Caribbean sea'
 }
 
-//----Task_1-----
+// //----Task_1-----
 
-
-console.log(person)
-
-//----Task_2-----
 for (let i of Object.entries(person)) {
     console.log(`${i[0]}: ${i[1]}`)
-}                                            // проще метода я не нашел, сначала сделал через двойной цикл получая отдельно ключ и значение
-                                             // затем нашел этот метод, ознакомился с ним
+}
+
+
+console.log('--------')
+
+// //----Task_2-----
+
+
+for (let item of Object.entries(person)) {
+    const key = item[0];
+    const value = item[1];
+    if (value == parseInt) {
+        console.log(`${key[0].toUpperCase()}${key.substr[1]}: ${value} age`)
+        continue
+    }
+
+    else if (key.includes('Name')) {
+        const twoStr = key.indexOf('Name')
+        console.log(`${key[0].toUpperCase()}${key.substr(1, twoStr - 1)} ${key.substr(twoStr).toLowerCase()}: ${value}`)
+    }
+
+    else if (key.includes('location')) {
+        console.log(`Where to find: ${value}`)
+    }
+
+    else if (typeof value === 'number') {
+        console.log(`${key[0].toUpperCase()}${key.substr(1)}: ${value} age`)
+    }
+}
