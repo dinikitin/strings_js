285db55b62dd71e31a0a218a420eae6fbfede79b
Dmitrii Nikitin <dnikitin.exceedteam@gmail.com>
2021-05-12 19:12:33 +0300

    Fixed shortcomings

--- simple_DOM/index.js
+++ simple_DOM/index.js
@@ -1,18 +1,26 @@
 const select = document.getElementById('select')
 const input = document.getElementById('input')
 const button = document.getElementById('btn')
+const standartColor = document.getElementsByClassName('bar')
 
-select.addEventListener('change', ()=>{
+input.value = select.value
+
+input.oninput = function() {
+    input.classList.remove('inputError')
+}
+
+select.addEventListener('change', () => {
     input.value = select.value
 })
 
-button.addEventListener('click', () =>{
+button.addEventListener('click', () => {
     Array.from(select.options).forEach((item) => {
         if (item.value === select.value)
             if (input.value === '') {
-                item.innerText = select.value
-            }
-            else 
+                const errorClass = document.getElementById('input')
+                errorClass.classList.add('inputError')
+            } else {
                 item.innerText = input.value
-        })
+            }
+    })
 })
